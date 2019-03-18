437e103c9c056b8e7b0e3ccfb6fb8847ab5b28f8
Viktar Dzmitryieu <Viktar_Dzmitryieu@epam.com>
2019-03-18 09:20:56 +0300

    added jest and eslint

---
+++ config/jest/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx}', '!src/index.jsx', '!src/store.js'],
+  moduleNameMapper: {
+    '\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '\\.(png|jpg)$': '<rootDir>/config/jest/__mocks__/fileMock.js',
+  },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  rootDir: '../../',
+};
