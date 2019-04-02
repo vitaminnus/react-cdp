32483d666bea2b3c9f1fea5ebd26cb1cb8864b3b
Viktar Dzmitryieu <Viktar_Dzmitryieu@epam.com>
2019-04-02 14:53:38 +0300

    task-4

--- src/components/App/__tests__/App.spec.jsx
+++ src/components/App/__tests__/App.spec.jsx
@@ -3,13 +3,15 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../index';
+import Film from '../../Film';
+import Button from '../../Button';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 const shallow = new ShallowRenderer();
 
 describe('App Snapshot', () => {
-  test('renders', () => {
+  test('App render', () => {
     const component = shallow.render(
       <App />,
     );
@@ -19,11 +21,20 @@ describe('App Snapshot', () => {
 
 
 describe('App logics', () => {
-  it('click to the film item', () => {
-    const wrapper = Enzyme.mount(
+  let wrapper;
+  beforeEach(() => {
+    wrapper = Enzyme.mount(
       <App />,
     );
-    wrapper.find('#film').first().simulate('click');
+  });
+  it('click to the film item should open details page', () => {
+    wrapper.find(Film).first().prop('onClick')();
     expect(wrapper.state('isActiveDetails')).toEqual(true);
+  });
+  it('click to the search button should open search page', () => {
+    wrapper.find(Film).first().prop('onClick')();
+    wrapper.update();
+    wrapper.find(Button).prop('onClick')();
+    expect(wrapper.state('isActiveSearch')).toEqual(true);
   });
 });
