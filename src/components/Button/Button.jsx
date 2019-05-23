289175016af33d028d04dc8febe1f02fc2f9fc26
Viktar Dzmitryieu <vitaminnus@gmail.com>
2019-05-23 17:10:35 +0300

    add styled components to the button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,25 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from './Button.scss';
+import styled from 'styled-components';
+import { COLOR, FONT } from '../../utils/consts';
+
+const StyledButton = styled.button.attrs(
+  props => ({
+    className: props.className,
+  }),
+)`
+  height: ${props => props.isMain ? '40px' : '30px'};
+  width: ${props => props.isMain ? '150px' : '100px'};
+  color: ${props => props.buttonType === 'white' ? COLOR['$light-red'] : 'white'};
+  text-align: center;
+  font-size: 14px;
+  font-family: ${FONT.default};
+  line-height: 22px;
+  cursor: pointer;
+  transition: all 0.3s;
+  margin: 10px;
+  outline: none;
+  text-transform: uppercase;
+  border: none;
+  background-color: ${props => props.buttonType === 'red' && COLOR['$light-red']};
+  background-color: ${props => props.buttonType === 'black' && COLOR['$dark-gray']};
+  background-color: ${props => props.buttonType === 'white' && 'white'};
+
+  &:hover {
+    cursor: pointer;
+    ${props => props.buttonType === 'red' && `background-color: lighten(${COLOR['$light-red']}, 20%);`}
+    ${props => props.buttonType === 'black' && 'color: darken(white, 20%);'}
+    ${props => props.buttonType === 'white' && 'color: darken(white, 20%);'}    
+  }
+`;
 
 const Button = ({
   id,
-  type,
+  type: buttonType,
   text,
   onClick,
   isMain,
   className,
   dataAttr,
 }) => (
-  <button
+  <StyledButton
     id={id}
-    className={`${styles.button} ${isMain ? styles.big : ''} ${styles[type]} ${className}`}
     type="button"
+    buttonType={buttonType}
     data-attr={dataAttr}
     onClick={onClick}
+    isMain={isMain}
+    className={className}
   >
     {text}
-  </button>
+  </StyledButton>
 );
 
 Button.propTypes = {
